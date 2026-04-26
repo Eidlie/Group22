38a3646734dd8bb691916184aa52dd4a67032ffa
Assume-R <asd1668454745@gmail.com>
2026-04-26 16:15:37 +0800

    Update NoteController.java

---
+++ notes-app/src/main/java/com/example/notes/controller/NoteController.java
@@ -0,0 +1,8 @@
+package com.example.notes.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoteController {
+    // TODO: Build REST endpoints (@PostMapping, @GetMapping) to connect to frontend
+}
