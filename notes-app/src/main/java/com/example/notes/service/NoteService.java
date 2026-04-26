21291d9f2d463a69a0d5026321e19c95c824555d
Assume-R <asd1668454745@gmail.com>
2026-04-26 16:16:17 +0800

    Update NoteService.java

---
+++ notes-app/src/main/java/com/example/notes/service/NoteService.java
@@ -0,0 +1,8 @@
+package com.example.notes.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+    // TODO: Implement in-memory database and CRUD methods later
+}
